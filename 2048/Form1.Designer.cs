9c2b7b6cbcb3ac4791694433cc9ae11eb5bb0a0d
Stefan <stefan5andonov@yahoo.com>
2017-05-24 02:48:39 +0200

    Started form design

---
+++ 2048/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace _2048
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.lbSubtitle = new System.Windows.Forms.Label();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label00 = new System.Windows.Forms.Label();
+            this.label01 = new System.Windows.Forms.Label();
+            this.label02 = new System.Windows.Forms.Label();
+            this.label03 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label23 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label33 = new System.Windows.Forms.Label();
+            this.label32 = new System.Windows.Forms.Label();
+            this.label31 = new System.Windows.Forms.Label();
+            this.label30 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitle.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbTitle.Location = new System.Drawing.Point(25, 24);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(132, 56);
+            this.lbTitle.TabIndex = 0;
+            this.lbTitle.Text = "2048";
+            // 
+            // lbSubtitle
+            // 
+            this.lbSubtitle.AutoSize = true;
+            this.lbSubtitle.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSubtitle.Location = new System.Drawing.Point(32, 80);
+            this.lbSubtitle.Name = "lbSubtitle";
+            this.lbSubtitle.Size = new System.Drawing.Size(254, 16);
+            this.lbSubtitle.TabIndex = 1;
+            this.lbSubtitle.Text = "Join the numbers and get to the 2048 tile!! ";
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(317, 70);
+            this.btnNewGame.Margin = new System.Windows.Forms.Padding(0);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(142, 37);
+            this.btnNewGame.TabIndex = 2;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label33);
+            this.panel1.Controls.Add(this.label32);
+            this.panel1.Controls.Add(this.label31);
+            this.panel1.Controls.Add(this.label30);
+            this.panel1.Controls.Add(this.label23);
+            this.panel1.Controls.Add(this.label13);
+            this.panel1.Controls.Add(this.label22);
+            this.panel1.Controls.Add(this.label12);
+            this.panel1.Controls.Add(this.label21);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.label20);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label03);
+            this.panel1.Controls.Add(this.label02);
+            this.panel1.Controls.Add(this.label01);
+            this.panel1.Controls.Add(this.label00);
+            this.panel1.Location = new System.Drawing.Point(77, 162);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(319, 330);
+            this.panel1.TabIndex = 3;
+            // 
+            // label00
+            // 
+            this.label00.AutoEllipsis = true;
+            this.label00.BackColor = System.Drawing.Color.RosyBrown;
+            this.label00.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label00.Location = new System.Drawing.Point(10, 10);
+            this.label00.Name = "label00";
+            this.label00.Size = new System.Drawing.Size(70, 70);
+            this.label00.TabIndex = 5;
+            // 
+            // label01
+            // 
+            this.label01.AutoEllipsis = true;
+            this.label01.BackColor = System.Drawing.Color.RosyBrown;
+            this.label01.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label01.Location = new System.Drawing.Point(86, 10);
+            this.label01.Name = "label01";
+            this.label01.Size = new System.Drawing.Size(70, 70);
+            this.label01.TabIndex = 6;
+            // 
+            // label02
+            // 
+            this.label02.AutoEllipsis = true;
+            this.label02.BackColor = System.Drawing.Color.RosyBrown;
+            this.label02.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label02.Location = new System.Drawing.Point(162, 10);
+            this.label02.Name = "label02";
+            this.label02.Size = new System.Drawing.Size(70, 70);
+            this.label02.TabIndex = 7;
+            // 
+            // label03
+            // 
+            this.label03.AutoEllipsis = true;
+            this.label03.BackColor = System.Drawing.Color.RosyBrown;
+            this.label03.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label03.Location = new System.Drawing.Point(237, 10);
+            this.label03.Name = "label03";
+            this.label03.Size = new System.Drawing.Size(70, 70);
+            this.label03.TabIndex = 8;
+            // 
+            // label13
+            // 
+            this.label13.AutoEllipsis = true;
+            this.label13.BackColor = System.Drawing.Color.RosyBrown;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(237, 89);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(70, 70);
+            this.label13.TabIndex = 12;
+            // 
+            // label12
+            // 
+            this.label12.AutoEllipsis = true;
+            this.label12.BackColor = System.Drawing.Color.RosyBrown;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(162, 89);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(70, 70);
+            this.label12.TabIndex = 11;
+            // 
+            // label11
+            // 
+            this.label11.AutoEllipsis = true;
+            this.label11.BackColor = System.Drawing.Color.RosyBrown;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(86, 89);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(70, 70);
+            this.label11.TabIndex = 10;
+            // 
+            // label10
+            // 
+            this.label10.AutoEllipsis = true;
+            this.label10.BackColor = System.Drawing.Color.RosyBrown;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(10, 89);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(70, 70);
+            this.label10.TabIndex = 9;
+            // 
+            // label23
+            // 
+            this.label23.AutoEllipsis = true;
+            this.label23.BackColor = System.Drawing.Color.RosyBrown;
+            this.label23.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label23.Location = new System.Drawing.Point(237, 168);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(70, 70);
+            this.label23.TabIndex = 12;
+            // 
+            // label22
+            // 
+            this.label22.AutoEllipsis = true;
+            this.label22.BackColor = System.Drawing.Color.RosyBrown;
+            this.label22.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label22.Location = new System.Drawing.Point(162, 168);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(70, 70);
+            this.label22.TabIndex = 11;
+            // 
+            // label21
+            // 
+            this.label21.AutoEllipsis = true;
+            this.label21.BackColor = System.Drawing.Color.RosyBrown;
+            this.label21.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label21.Location = new System.Drawing.Point(86, 168);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(70, 70);
+            this.label21.TabIndex = 10;
+            // 
+            // label20
+            // 
+            this.label20.AutoEllipsis = true;
+            this.label20.BackColor = System.Drawing.Color.RosyBrown;
+            this.label20.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label20.Location = new System.Drawing.Point(10, 168);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(70, 70);
+            this.label20.TabIndex = 9;
+            // 
+            // label33
+            // 
+            this.label33.AutoEllipsis = true;
+            this.label33.BackColor = System.Drawing.Color.RosyBrown;
+            this.label33.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label33.Location = new System.Drawing.Point(237, 247);
+            this.label33.Name = "label33";
+            this.label33.Size = new System.Drawing.Size(70, 70);
+            this.label33.TabIndex = 16;
+            // 
+            // label32
+            // 
+            this.label32.AutoEllipsis = true;
+            this.label32.BackColor = System.Drawing.Color.RosyBrown;
+            this.label32.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label32.Location = new System.Drawing.Point(162, 247);
+            this.label32.Name = "label32";
+            this.label32.Size = new System.Drawing.Size(70, 70);
+            this.label32.TabIndex = 15;
+            // 
+            // label31
+            // 
+            this.label31.AutoEllipsis = true;
+            this.label31.BackColor = System.Drawing.Color.RosyBrown;
+            this.label31.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label31.Location = new System.Drawing.Point(86, 247);
+            this.label31.Name = "label31";
+            this.label31.Size = new System.Drawing.Size(70, 70);
+            this.label31.TabIndex = 14;
+            // 
+            // label30
+            // 
+            this.label30.AutoEllipsis = true;
+            this.label30.BackColor = System.Drawing.Color.RosyBrown;
+            this.label30.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label30.Location = new System.Drawing.Point(10, 247);
+            this.label30.Name = "label30";
+            this.label30.Size = new System.Drawing.Size(70, 70);
+            this.label30.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(484, 662);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.lbSubtitle);
+            this.Controls.Add(this.lbTitle);
+            this.Name = "Form1";
+            this.Text = "2048 Game";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTitle;
+        private System.Windows.Forms.Label lbSubtitle;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label33;
+        private System.Windows.Forms.Label label32;
+        private System.Windows.Forms.Label label31;
+        private System.Windows.Forms.Label label30;
+        private System.Windows.Forms.Label label23;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label03;
+        private System.Windows.Forms.Label label02;
+        private System.Windows.Forms.Label label01;
+        private System.Windows.Forms.Label label00;
+    }
+}
+
---
+++ 2048/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _2048
+{
+    public partial class Form1 : Form
+    {
+        /*public static List<Control> FilterControls(Control root) {
+            if (root == null)
+                return null;
+            List<Control> controls = new List<Control>();
+            Stack<Control> stack = new Stack<Control>();
+
+            stack.Push(root);
+            controls.Add(root);
+
+            while (stack.Any()) {
+                var next = stack.Pop();
+                //controls.Add(next);
+                foreach (Control c1 in next.Controls)
+                    stack.Push(c1);
+            }
+
+            
+
+            return controls;
+            
+        }*/
+
+        public static IEnumerable<Control> GetAllControls(Control root)
+        {
+            var stack = new Stack<Control>();
+            stack.Push(root);
+
+            while (stack.Any())
+            {
+                var next = stack.Pop();
+                foreach (Control child in next.Controls)
+                    stack.Push(child);
+
+                yield return next;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.FromArgb(253, 247, 237);
+            this.lbTitle.ForeColor = Color.FromArgb(118, 114, 103);
+            this.lbSubtitle.ForeColor = Color.FromArgb(118, 114, 103);
+            this.btnNewGame.ForeColor = Color.White;
+            this.btnNewGame.BackColor = Color.FromArgb(118, 114, 103);
+            panel1.BackColor = Color.FromArgb(118, 114, 103);
+            label01.BackColor = Color.FromArgb(205, 193, 179);
+
+            label00.Text = this.Controls.Count.ToString();
+            label01.Text = this.Controls.ToString();
+
+            List<Control> controls = new List<Control>();
+
+            foreach (Control c in this.Controls) {
+                controls.AddRange(GetAllControls(c));
+            }
+
+            foreach (Control c in controls) { 
+                if (c.Name.StartsWith("label"))
+                    c.BackColor = Color.FromArgb(205, 193, 179);
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
