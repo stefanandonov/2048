7aa5b5349904b3ee6b597bc800849879bebe5f55
Stefan <stefan5andonov@yahoo.com>
2017-05-25 02:11:42 +0200

    Random initialisation finished

--- 2048/Form1.Designer.cs
+++ 2048/Form1.Designer.cs
@@ -32,22 +32,22 @@
             this.lbSubtitle = new System.Windows.Forms.Label();
             this.btnNewGame = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label00 = new System.Windows.Forms.Label();
-            this.label01 = new System.Windows.Forms.Label();
-            this.label02 = new System.Windows.Forms.Label();
-            this.label03 = new System.Windows.Forms.Label();
-            this.label13 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label23 = new System.Windows.Forms.Label();
-            this.label22 = new System.Windows.Forms.Label();
-            this.label21 = new System.Windows.Forms.Label();
-            this.label20 = new System.Windows.Forms.Label();
             this.label33 = new System.Windows.Forms.Label();
             this.label32 = new System.Windows.Forms.Label();
             this.label31 = new System.Windows.Forms.Label();
             this.label30 = new System.Windows.Forms.Label();
+            this.label23 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label03 = new System.Windows.Forms.Label();
+            this.label02 = new System.Windows.Forms.Label();
+            this.label01 = new System.Windows.Forms.Label();
+            this.label00 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -105,126 +105,6 @@
             this.panel1.Size = new System.Drawing.Size(319, 330);
             this.panel1.TabIndex = 3;
             // 
-            // label00
-            // 
-            this.label00.AutoEllipsis = true;
-            this.label00.BackColor = System.Drawing.Color.RosyBrown;
-            this.label00.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label00.Location = new System.Drawing.Point(10, 10);
-            this.label00.Name = "label00";
-            this.label00.Size = new System.Drawing.Size(70, 70);
-            this.label00.TabIndex = 5;
-            // 
-            // label01
-            // 
-            this.label01.AutoEllipsis = true;
-            this.label01.BackColor = System.Drawing.Color.RosyBrown;
-            this.label01.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label01.Location = new System.Drawing.Point(86, 10);
-            this.label01.Name = "label01";
-            this.label01.Size = new System.Drawing.Size(70, 70);
-            this.label01.TabIndex = 6;
-            // 
-            // label02
-            // 
-            this.label02.AutoEllipsis = true;
-            this.label02.BackColor = System.Drawing.Color.RosyBrown;
-            this.label02.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label02.Location = new System.Drawing.Point(162, 10);
-            this.label02.Name = "label02";
-            this.label02.Size = new System.Drawing.Size(70, 70);
-            this.label02.TabIndex = 7;
-            // 
-            // label03
-            // 
-            this.label03.AutoEllipsis = true;
-            this.label03.BackColor = System.Drawing.Color.RosyBrown;
-            this.label03.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label03.Location = new System.Drawing.Point(237, 10);
-            this.label03.Name = "label03";
-            this.label03.Size = new System.Drawing.Size(70, 70);
-            this.label03.TabIndex = 8;
-            // 
-            // label13
-            // 
-            this.label13.AutoEllipsis = true;
-            this.label13.BackColor = System.Drawing.Color.RosyBrown;
-            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label13.Location = new System.Drawing.Point(237, 89);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(70, 70);
-            this.label13.TabIndex = 12;
-            // 
-            // label12
-            // 
-            this.label12.AutoEllipsis = true;
-            this.label12.BackColor = System.Drawing.Color.RosyBrown;
-            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.Location = new System.Drawing.Point(162, 89);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(70, 70);
-            this.label12.TabIndex = 11;
-            // 
-            // label11
-            // 
-            this.label11.AutoEllipsis = true;
-            this.label11.BackColor = System.Drawing.Color.RosyBrown;
-            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(86, 89);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(70, 70);
-            this.label11.TabIndex = 10;
-            // 
-            // label10
-            // 
-            this.label10.AutoEllipsis = true;
-            this.label10.BackColor = System.Drawing.Color.RosyBrown;
-            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(10, 89);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(70, 70);
-            this.label10.TabIndex = 9;
-            // 
-            // label23
-            // 
-            this.label23.AutoEllipsis = true;
-            this.label23.BackColor = System.Drawing.Color.RosyBrown;
-            this.label23.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label23.Location = new System.Drawing.Point(237, 168);
-            this.label23.Name = "label23";
-            this.label23.Size = new System.Drawing.Size(70, 70);
-            this.label23.TabIndex = 12;
-            // 
-            // label22
-            // 
-            this.label22.AutoEllipsis = true;
-            this.label22.BackColor = System.Drawing.Color.RosyBrown;
-            this.label22.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label22.Location = new System.Drawing.Point(162, 168);
-            this.label22.Name = "label22";
-            this.label22.Size = new System.Drawing.Size(70, 70);
-            this.label22.TabIndex = 11;
-            // 
-            // label21
-            // 
-            this.label21.AutoEllipsis = true;
-            this.label21.BackColor = System.Drawing.Color.RosyBrown;
-            this.label21.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label21.Location = new System.Drawing.Point(86, 168);
-            this.label21.Name = "label21";
-            this.label21.Size = new System.Drawing.Size(70, 70);
-            this.label21.TabIndex = 10;
-            // 
-            // label20
-            // 
-            this.label20.AutoEllipsis = true;
-            this.label20.BackColor = System.Drawing.Color.RosyBrown;
-            this.label20.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label20.Location = new System.Drawing.Point(10, 168);
-            this.label20.Name = "label20";
-            this.label20.Size = new System.Drawing.Size(70, 70);
-            this.label20.TabIndex = 9;
-            // 
             // label33
             // 
             this.label33.AutoEllipsis = true;
@@ -234,6 +114,7 @@
             this.label33.Name = "label33";
             this.label33.Size = new System.Drawing.Size(70, 70);
             this.label33.TabIndex = 16;
+            this.label33.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label32
             // 
@@ -244,6 +125,7 @@
             this.label32.Name = "label32";
             this.label32.Size = new System.Drawing.Size(70, 70);
             this.label32.TabIndex = 15;
+            this.label32.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label31
             // 
@@ -254,6 +136,7 @@
             this.label31.Name = "label31";
             this.label31.Size = new System.Drawing.Size(70, 70);
             this.label31.TabIndex = 14;
+            this.label31.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label30
             // 
@@ -264,6 +147,139 @@
             this.label30.Name = "label30";
             this.label30.Size = new System.Drawing.Size(70, 70);
             this.label30.TabIndex = 13;
+            this.label30.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label23
+            // 
+            this.label23.AutoEllipsis = true;
+            this.label23.BackColor = System.Drawing.Color.RosyBrown;
+            this.label23.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label23.Location = new System.Drawing.Point(237, 168);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(70, 70);
+            this.label23.TabIndex = 12;
+            this.label23.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label13
+            // 
+            this.label13.AutoEllipsis = true;
+            this.label13.BackColor = System.Drawing.Color.RosyBrown;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(237, 89);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(70, 70);
+            this.label13.TabIndex = 12;
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label22
+            // 
+            this.label22.AutoEllipsis = true;
+            this.label22.BackColor = System.Drawing.Color.RosyBrown;
+            this.label22.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label22.Location = new System.Drawing.Point(162, 168);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(70, 70);
+            this.label22.TabIndex = 11;
+            this.label22.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            this.label12.AutoEllipsis = true;
+            this.label12.BackColor = System.Drawing.Color.RosyBrown;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(162, 89);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(70, 70);
+            this.label12.TabIndex = 11;
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label21
+            // 
+            this.label21.AutoEllipsis = true;
+            this.label21.BackColor = System.Drawing.Color.RosyBrown;
+            this.label21.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label21.Location = new System.Drawing.Point(86, 168);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(70, 70);
+            this.label21.TabIndex = 10;
+            this.label21.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            this.label11.AutoEllipsis = true;
+            this.label11.BackColor = System.Drawing.Color.RosyBrown;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(86, 89);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(70, 70);
+            this.label11.TabIndex = 10;
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label20
+            // 
+            this.label20.AutoEllipsis = true;
+            this.label20.BackColor = System.Drawing.Color.RosyBrown;
+            this.label20.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label20.Location = new System.Drawing.Point(10, 168);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(70, 70);
+            this.label20.TabIndex = 9;
+            this.label20.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.AutoEllipsis = true;
+            this.label10.BackColor = System.Drawing.Color.RosyBrown;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(10, 89);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(70, 70);
+            this.label10.TabIndex = 9;
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label03
+            // 
+            this.label03.AutoEllipsis = true;
+            this.label03.BackColor = System.Drawing.Color.RosyBrown;
+            this.label03.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label03.Location = new System.Drawing.Point(237, 10);
+            this.label03.Name = "label03";
+            this.label03.Size = new System.Drawing.Size(70, 70);
+            this.label03.TabIndex = 8;
+            this.label03.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label02
+            // 
+            this.label02.AutoEllipsis = true;
+            this.label02.BackColor = System.Drawing.Color.RosyBrown;
+            this.label02.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label02.Location = new System.Drawing.Point(162, 10);
+            this.label02.Name = "label02";
+            this.label02.Size = new System.Drawing.Size(70, 70);
+            this.label02.TabIndex = 7;
+            this.label02.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label01
+            // 
+            this.label01.AutoEllipsis = true;
+            this.label01.BackColor = System.Drawing.Color.RosyBrown;
+            this.label01.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label01.Location = new System.Drawing.Point(86, 10);
+            this.label01.Name = "label01";
+            this.label01.Size = new System.Drawing.Size(70, 70);
+            this.label01.TabIndex = 6;
+            this.label01.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label00
+            // 
+            this.label00.AutoEllipsis = true;
+            this.label00.BackColor = System.Drawing.Color.RosyBrown;
+            this.label00.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label00.Location = new System.Drawing.Point(10, 10);
+            this.label00.Name = "label00";
+            this.label00.Size = new System.Drawing.Size(70, 70);
+            this.label00.TabIndex = 5;
+            this.label00.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
--- 2048/Form1.cs
+++ 2048/Form1.cs
@@ -11,27 +11,63 @@ namespace _2048
 {
     public partial class Form1 : Form
     {
-        /*public static List<Control> FilterControls(Control root) {
-            if (root == null)
-                return null;
+        public int[][] matrix { set; get; }
+        public bool[][] filled { set; get; }
+        public List<Control> controls = new List<Control>();
+
+        
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            matrix = new int [4][];
+            filled = new bool[4][];
+            
+
+
+            for (int i=0;i<4;i++){
+                matrix[i]=new int [4];
+                filled[i]=new bool[4];
+
+                for (int j=0;j<4;j++){
+                    matrix[i][j] = 0;
+                    filled[i][j] = false;
+                }
+                    
+            }
+            this.BackColor = Color.FromArgb(253, 247, 237);
+            this.lbTitle.ForeColor = Color.FromArgb(118, 114, 103);
+            this.lbSubtitle.ForeColor = Color.FromArgb(118, 114, 103);
+            this.btnNewGame.ForeColor = Color.White;
+            this.btnNewGame.BackColor = Color.FromArgb(118, 114, 103);
+            panel1.BackColor = Color.FromArgb(118, 114, 103);
+            label01.BackColor = Color.FromArgb(205, 193, 179);
+
+            //label00.Text = this.Controls.Count.ToString();
+            //label01.Text = this.Controls.ToString();
+            //label01.Text = matrix[0][1].ToString();
+
             List<Control> controls = new List<Control>();
-            Stack<Control> stack = new Stack<Control>();
 
-            stack.Push(root);
-            controls.Add(root);
-
-            while (stack.Any()) {
-                var next = stack.Pop();
-                //controls.Add(next);
-                foreach (Control c1 in next.Controls)
-                    stack.Push(c1);
+            foreach (Control c in this.Controls) {
+                controls.AddRange(GetAllControls(c));
+                
             }
 
-            
+           
 
-            return controls;
+            foreach (Control c in controls) { 
+                if (c.Name.StartsWith("label"))
+                    c.BackColor = Color.FromArgb(205, 193, 179);
+            }
+
+            generateStart();
+            showNumbers();
             
-        }*/
+            
+        }
 
         public static IEnumerable<Control> GetAllControls(Control root)
         {
@@ -48,31 +84,78 @@ namespace _2048
             }
         }
 
-        public Form1()
-        {
-            InitializeComponent();
-            this.BackColor = Color.FromArgb(253, 247, 237);
-            this.lbTitle.ForeColor = Color.FromArgb(118, 114, 103);
-            this.lbSubtitle.ForeColor = Color.FromArgb(118, 114, 103);
-            this.btnNewGame.ForeColor = Color.White;
-            this.btnNewGame.BackColor = Color.FromArgb(118, 114, 103);
-            panel1.BackColor = Color.FromArgb(118, 114, 103);
-            label01.BackColor = Color.FromArgb(205, 193, 179);
-
-            label00.Text = this.Controls.Count.ToString();
-            label01.Text = this.Controls.ToString();
-
-            List<Control> controls = new List<Control>();
-
-            foreach (Control c in this.Controls) {
-                controls.AddRange(GetAllControls(c));
-            }
-
-            foreach (Control c in controls) { 
-                if (c.Name.StartsWith("label"))
-                    c.BackColor = Color.FromArgb(205, 193, 179);
-            }
+        private void generateStart() { //funkcija koja generira 2 broja (2 ili 4) na 2 razlicni pozicii
+            Random rdm = new Random();
+            int i = 0;
+            while (i < 2) {
+                int x = rdm.Next(0, 4);
+                int y = rdm.Next(0, 4);
+                while (filled[x][y] == true) {
+                    x = rdm.Next(0, 4);
+                    y = rdm.Next(0, 4);
+                }
+                int nmbr=rdm.Next(2,5);
+                while (nmbr == 3) {
+                    nmbr = rdm.Next(2, 5);
+                }
+                matrix[x][y] = nmbr;
+                filled[x][y] = true;
+                ++i;
+            }          
             
+        }
+
+        private void showNumbers() {
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    if (matrix[i][j] != 0) {
+                        showNumber(i, j);
+                    }
+                }
+            }
+        }
+
+        private void showNumber(int i, int j) {
+            if (i == 0) { 
+                if (j==0) 
+                    label00.Text = matrix[i][j].ToString();
+                else if (j == 1)
+                    label01.Text = matrix[i][j].ToString();
+                else if (j == 2)
+                    label02.Text = matrix[i][j].ToString();
+                else if (j == 3)
+                    label03.Text = matrix[i][j].ToString();
+            }
+            else if (i == 1) {
+                if (j == 0)
+                    label10.Text = matrix[i][j].ToString();
+                else if (j == 1)
+                    label11.Text = matrix[i][j].ToString();
+                else if (j == 2)
+                    label12.Text = matrix[i][j].ToString();
+                else if (j == 3)
+                    label13.Text = matrix[i][j].ToString();
+            }
+            else if (i == 2) {
+                if (j == 0)
+                    label20.Text = matrix[i][j].ToString();
+                else if (j == 1)
+                    label21.Text = matrix[i][j].ToString();
+                else if (j == 2)
+                    label22.Text = matrix[i][j].ToString();
+                else if (j == 3)
+                    label23.Text = matrix[i][j].ToString();
+            }
+            else if (i == 3) {
+                if (j == 0)
+                    label30.Text = matrix[i][j].ToString();
+                else if (j == 1)
+                    label31.Text = matrix[i][j].ToString();
+                else if (j == 2)
+                    label32.Text = matrix[i][j].ToString();
+                else if (j == 3)
+                    label33.Text = matrix[i][j].ToString();
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
